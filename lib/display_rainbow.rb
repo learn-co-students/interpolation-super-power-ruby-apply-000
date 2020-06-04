ef5878fb4c12c4479bec2cb76f5cef37ca641e72
Sawandi Wilson <sawandiwilson@gmail.com>
2020-06-04 19:32:55 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,14 +1,20 @@
-def display_rainbow 
-display_rainbow = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
-display_rainbow
-def colors
-colors= #{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]}
-display_rainbow(colors)
-puts "The first color is: #{colors[0]}"
-puts "The second color is: #{colors[1]}"
-puts "The third color is: #{colors[2]}"
-puts "The fourth color is: #{colors[3]}"
-puts "The fifth color is: #{colors[4]}"
-puts "The sixth color is: #{colors[5]}"
-puts "The fifth color is: #{colors[6]}"
+require "pry"
+# def display_rainbow 
+# display_rainbow = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
+# display_rainbow
+# def colors
+# colors= #{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]}
+# display_rainbow(colors)
+# puts "The first color is: #{colors[0]}"
+# puts "The second color is: #{colors[1]}"
+# puts "The third color is: #{colors[2]}"
+# puts "The fourth color is: #{colors[3]}"
+# puts "The fifth color is: #{colors[4]}"
+# puts "The sixth color is: #{colors[5]}"
+# puts "The fifth color is: #{colors[6]}"
+# end
+
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
