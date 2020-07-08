026bf80c90814906592e0fbe1026422df1093182
candescent-method-1839 <mqu000@citymail.cuny.edu>
2020-07-08 14:26:19 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  color1 = colors[0][0,1].upcase
+  color2 = colors[1][0,1].upcase
+  color3 = colors[2][0,1].upcase
+  color4 = colors[3][0,1].upcase
+  color5 = colors[4][0,1].upcase
+  color6 = colors[5][0,1].upcase
+  color7 = colors[6][0,1].upcase
+  puts "#{color1}: #{colors[0]}, #{color2}: #{colors[1]}, #{color3}: #{colors[2]}, #{color4}: #{colors[3]}, #{color5}: #{colors[4]}, #{color6}: #{colors[5]}, #{color7}: #{colors[6]}"
+end
