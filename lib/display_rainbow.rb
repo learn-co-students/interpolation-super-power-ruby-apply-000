6121718d76b735c664fd75b40ddbac7cc7608345
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-07-30 04:23:07 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+end
+
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+puts "The order of rainbow's colors are: R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}."
+
+
+
+
+
