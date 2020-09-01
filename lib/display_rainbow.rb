1ce328364793e32dafd873792e7b1543702b4673
John Orozco <john.o111@yahoo.com>
2020-09-01 02:53:30 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,13 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors=[])
  letters = ["R: ","O: ","Y: ","G: ","B: ","I: ","V: "]
- red = letters[0] + colors[0]
- orange = letters[1] + colors[1]
- yellow = letters[2] + colors[2]
- green = letters[3] + colors[3]
- blue = letters[4] + colors[4]
- indigo = letters[5] + colors[5]
+ space = ", "
+ red = letters[0] + colors[0] + space
+ orange = letters[1] + colors[1] + space
+ yellow = letters[2] + colors[2] + space
+ green = letters[3] + colors[3] + space
+ blue = letters[4] + colors[4] + space
+ indigo = letters[5] + colors[5] + space
  violet = letters[6] + colors[6]
- puts red + ", " + orange + ", " + yellow + ", " + green + ", " + blue + ", " + indigo + ", " + violet
+ puts red + orange + yellow + green + blue + indigo + violet
  end
